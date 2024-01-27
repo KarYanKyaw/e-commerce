407def221b21c638503f668df09436b49b17e500
KarYanKyaw <karyankyaw02@gmail.com>
2024-01-27 13:28:06 +0630

    error page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { Home, ProductDetail } from "./components/pages";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, NotFoundPage, ProductDetail } from "./components/pages";
 import { Nav } from "./components/ui";
 import Footer from "./components/ui/Footer.ui";
 
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productDetail/:id" element={<ProductDetail />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </>
